f469fb7fa296d87f6af904f1f97b26057414d92b
Darren-Choo <97815742+Darren-Choo@users.noreply.github.com>
2022-03-23 11:50:18 +0800

    Update Payment.java

--- SC2002-PROJECT/src/Payment.java
+++ SC2002-PROJECT/src/Payment.java
@@ -1,8 +1,6 @@
-enum methodOfPayment{
-	Cash,
-	Card // Can add Debit and Credit separately later on if you guys want to
-}
+
 public class Payment {
+	public enum methodOfPayment{Cash, Card} // Can add Debit and Credit separately later on if you guys want to
 	public double roomCharges;
 	public double tax;
 	public double roomServices;
