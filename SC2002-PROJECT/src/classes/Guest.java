02eccd7f072e081fa245af714776554cbbb266d5
Darren-Choo <darren_ryu@hotmail.com>
2022-04-14 15:58:43 +0800

    -

--- SC2002-PROJECT/src/classes/Guest.java
+++ SC2002-PROJECT/src/classes/Guest.java
@@ -1,19 +1,62 @@
 package classes;
 
+/**
+ * @author Darren Choo
+ * @version 1.0
+ * @since 14th April 2022
+ */
 
 public class Guest {
-	
+	/**
+     * guest's name
+     */
 	private String name;
-	private String creditCardName; 
+	/**
+     * guest's credit card name
+     */
+	private String creditCardName;
+	/**
+     * guest's credit card number
+     */
 	private String creditCardNumber;
+	/**
+     * guest's address
+     */
 	private String address;
+	/**
+     * guest's country
+     */
 	private String country;
+	/**
+     * guest's gender
+     */
 	private char gender;
+	/**
+     * guest's identitfication number
+     */
 	private String identity;
+	/**
+     * guest's nationality
+     */
 	private String nationality;
-	private String contact; // Needs to have country code
+	/**
+     * guest's contact number
+     */
+	private String contact;
 	
 	
+	/**
+     * constructor of guest
+     * @param name name of guest
+     * @param creditCardName guest's credit card name
+     * @param creditCardNumber guest's credit card name
+     * @param address guest's address
+     * @param country guest's country
+     * @param gender guest's gender
+     * @param identity guest's identitfication number
+     * @param nationality guest's nationality
+     * @param contact guest's contact number
+     */
 	public Guest(String name, String creditCardName, String creditCardNumber, String address, String country, char gender, String identity, String nationality, String contact)
 	{
 		this.name = name;
@@ -27,92 +70,146 @@ public class Guest {
 		this.contact = contact;
 	}
 
-
+	/**
+     * gets the name of the guest
+     * @return Name of guest
+     */
 	public String getName() {
 		return name;
 	}
 
-
+	/**
+	 * sets the name of the guest
+	 * @param name name of the guest
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
+	/**
+     * gets the guest's credit card name
+     * @return guest's credit card name
+     */
 	public String getCreditCardName() {
 		return creditCardName;
 	}
 
-
+	/**
+	 * sets guest's credit card name
+	 * @param creditCardName guest's credit card name
+	 */
 	public void setCreditCardName(String creditCardName) {
 		this.creditCardName = creditCardName;
 	}
 
-
+	/**
+	 * get guest's credit card number
+	 * @return guest's credit card number
+	 */
 	public String getCreditCardNumber() {
 		return creditCardNumber;
 	}
 
-
+	/**
+	 * sets guest's credit card number
+	 * @param creditCardNumber guest's credit card number
+	 */
 	public void setCreditCardNumber(String creditCardNumber) {
 		this.creditCardNumber = creditCardNumber;
 	}
 
-
+	/**
+	 * gets guest's address
+	 * @return guest's address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
-
+	/**
+	 * sets guest's address
+	 * @param address guest's address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
-
+	/**
+	 * gets guest's country
+	 * @return guest's country
+	 */
 	public String getCountry() {
 		return country;
 	}
 
-
+	/**
+	 * sets guest's country
+	 * @param country guest's country
+	 */
 	public void setCountry(String country) {
 		this.country = country;
 	}
 
-
+	/**
+	 * gets guest's gender
+	 * @return guest's gender
+	 */
 	public char getGender() {
 		return gender;
 	}
 
-
+	/**
+	 * sets guest's gender
+	 * @param gender guest's gender
+	 */
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
 
-
+	/**
+	 * gets guest's identity number
+	 * @return guest's identity number
+	 */
 	public String getIdentity() {
 		return identity;
 	}
 
-
+	/**
+	 * sets guest's identity number
+	 * @param identity guest's identity number
+	 */
 	public void setIdentity(String identity) {
 		this.identity = identity;
 	}
 
-
+	/**
+	 * gets guest's nationality
+	 * @return guest's nationality
+	 */
 	public String getNationality() {
 		return nationality;
 	}
 
-
+	/**
+	 * sets guest's nationality
+	 * @param nationality guest's nationality
+	 */
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
 
-
+	/**
+	 * gets guest's contact number
+	 * @return guest's contact number
+	 */
 	public String getContact() {
 		return contact;
 	}
 
-
+	/**
+	 * sets guest's contact number
+	 * @param contact guest's contact number
+	 */
 	public void setContact(String contact) {
 		this.contact = contact;
 	}
--- SC2002-PROJECT/src/classes/Payment.java
+++ SC2002-PROJECT/src/classes/Payment.java
@@ -2,24 +2,89 @@ package classes;
 
 import java.util.ArrayList;
 
+/**
+ * @author Darren Choo
+ * @version 1.0
+ * @since 14th April 2022
+ */
 public class Payment {
-	public enum methodOfPayment{CASH, CARD}
-	
+	/**
+	 * possible status values of payment
+	 */
+	public enum methodOfPayment{
+		/**
+		 * method of payment is cash
+		 */
+		CASH, 
+		/**
+		 * method of payment is card
+		 */
+		CARD}
+	/**
+	 * unique identifier of payment
+	 */
 	public int paymentID;
+	/**
+	 * room charges for all the nights
+	 */
 	public double roomCharges;
+	/**
+	 * tax on total charges
+	 */
 	public double tax;
+	/**
+	 * room service charges
+	 */
 	public double roomServices;
+	/**
+	 * discount available
+	 */
 	public double discount;
+	/**
+	 * total charges
+	 */
 	public double totalCharges;
+	/**
+	 * method of payment
+	 */
 	public methodOfPayment paymentMethod;
+	/**
+	 * array list of orders for a room
+	 */
 	public ArrayList<Order> orderThisRoom;
+	/**
+	 * number of nights stayed
+	 */
 	public int numberOfNights;
+	/**
+	 * guest's credit card name
+	 */
 	public String CreditCardName;
+	/**
+	 * guest's billing address
+	 */
 	public String billingAddress;
+	/**
+	 * guest's billing number
+	 */
 	public String billingCardNumber;
 	
 	
-	
+	/**
+	 * 
+	 * @param paymentID unique identifier of payment
+	 * @param roomCharges room charges for all the nights
+	 * @param tax tax on total charges
+	 * @param roomServices room service charges
+	 * @param discount discount available
+	 * @param totalCharges total charges
+	 * @param paymentMethod method of payment
+	 * @param orderThisRoom array list of orders for a room
+	 * @param numberOfNights number of nights stayed
+	 * @param creditCardName guest's credit card name
+	 * @param billingAddress guest's billing address
+	 * @param billingCardNumber guest's billing number
+	 */
 	public Payment(int paymentID, double roomCharges, double tax, double roomServices, double discount,
 			double totalCharges, methodOfPayment paymentMethod, ArrayList<Order> orderThisRoom, int numberOfNights,
 			String creditCardName, String billingAddress, String billingCardNumber) {
@@ -39,145 +104,217 @@ public class Payment {
 	}
 
 
-
+	/**
+	 * gets the unique identifier of payment
+	 * @return unique identifier of payment
+	 */
 	public int getPaymentID() {
 		return paymentID;
 	}
 
 
-
+	/**
+	 * sets the unique identifier of payment
+	 * @param paymentID unique identifier of payment
+	 */
 	public void setPaymentID(int paymentID) {
 		this.paymentID = paymentID;
 	}
 
 
-
+	/**
+	 * gets the room charges of the room
+	 * @return room charges of the room
+	 */
 	public double getRoomCharges() {
 		return roomCharges;
 	}
 
 
-
+	/**
+	 * sets the room charges of the room
+	 * @param roomCharges room charges of the room
+	 */
 	public void setRoomCharges(double roomCharges) {
 		this.roomCharges = roomCharges;
 	}
 
 
-
+	/**
+	 * gets the tax on total charges
+	 * @return tax on total charges
+	 */
 	public double getTax() {
 		return tax;
 	}
 
 
-
+	/**
+	 * sets the tax on total charges
+	 * @param tax tax on total charges
+	 */
 	public void setTax(double tax) {
 		this.tax = tax;
 	}
 
 
-
+	/**
+	 * gets the room service charges
+	 * @return room service charges
+	 */
 	public double getRoomServices() {
 		return roomServices;
 	}
 
 
-
+	/**
+	 * sets room service charges
+	 * @param roomServices room service charges
+	 */
 	public void setRoomServices(double roomServices) {
 		this.roomServices = roomServices;
 	}
 
 
-
+	/**
+	 * gets the discounts available
+	 * @return discounts available
+	 */
 	public double getDiscount() {
 		return discount;
 	}
 
 
-
+	/**
+	 * sets the discounts available
+	 * @param discount discounts available
+	 */
 	public void setDiscount(double discount) {
 		this.discount = discount;
 	}
 
 
-
+	/**
+	 * gets the total charges
+	 * @return total charges
+	 */
 	public double getTotalCharges() {
 		return totalCharges;
 	}
 
 
-
+	/**
+	 * sets the total charges
+	 * @param totalCharges total charges
+	 */
 	public void setTotalCharges(double totalCharges) {
 		this.totalCharges = totalCharges;
 	}
 
 
-
+	/**
+	 * gets the method of payment
+	 * @return method of payment
+	 */
 	public methodOfPayment getPaymentMethod() {
 		return paymentMethod;
 	}
 
 
-
+	/**
+	 * sets the method of payment
+	 * @param paymentMethod method of payment
+	 */
 	public void setPaymentMethod(methodOfPayment paymentMethod) {
 		this.paymentMethod = paymentMethod;
 	}
 
 
-
+	/**
+	 * gets the array list of orders for a room
+	 * @return array list of orders for a room
+	 */
 	public ArrayList<Order> getOrderThisRoom() {
 		return orderThisRoom;
 	}
 
 
-
+	/**
+	 * sets the array list of orders for a room
+	 * @param orderThisRoom array list of orders for a room
+	 */
 	public void setOrderThisRoom(ArrayList<Order> orderThisRoom) {
 		this.orderThisRoom = orderThisRoom;
 	}
 
 
-
+	/**
+	 * gets the number of nights stayed
+	 * @return number of nights stayed
+	 */
 	public int getNumberOfNights() {
 		return numberOfNights;
 	}
 
 
-
+	/**
+	 * sets the number of nights stayed
+	 * @param numberOfNights number of nights stayed
+	 */
 	public void setNumberOfNights(int numberOfNights) {
 		this.numberOfNights = numberOfNights;
 	}
 
 
-
+	/**
+	 * gets the guest's credit card name
+	 * @return guest's credit card name
+	 */
 	public String getCreditCardName() {
 		return CreditCardName;
 	}
 
 
-
+	/**
+	 * sets the guest's credit card name
+	 * @param creditCardName guest's credit card name
+	 */
 	public void setCreditCardName(String creditCardName) {
 		CreditCardName = creditCardName;
 	}
 
 
-
+	/**
+	 * gets the guest's billing address
+	 * @return guest's billing address
+	 */
 	public String getBillingAddress() {
 		return billingAddress;
 	}
 
 
-
+	/**
+	 * sets the guest's billing address
+	 * @param billingAddress guest's billing address
+	 */
 	public void setBillingAddress(String billingAddress) {
 		this.billingAddress = billingAddress;
 	}
 
 
-
+	/**
+	 * gets the guest's billing card number
+	 * @return guest's billing card number
+	 */
 	public String getBillingCardNumber() {
 		return billingCardNumber;
 	}
 
 
-
+	/**
+	 * sets the guest's billing card number
+	 * @param billingCardNumber guest's billing card number
+	 */
 	public void setBillingCardNumber(String billingCardNumber) {
 		this.billingCardNumber = billingCardNumber;
 	}
